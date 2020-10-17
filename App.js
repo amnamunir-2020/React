9538329c8aebde01007d2ad257cca0ac877cf512
amnamunir-2020 <59399151+amnamunir-2020@users.noreply.github.com>
2020-10-18 01:57:11 +0500

    Update App.js

--- App.js
+++ App.js
@@ -29,6 +29,8 @@ export default App;
 //   render(){
 //     return(
 
+ //jsx javascrpt extensible
+
 //       <div>
 //         <h1>Asslamoalaikum Warhmatullahi Wabarakatuhu!</h1>
 //       </div>
@@ -38,13 +40,61 @@ export default App;
 //export default App
 
 //FUNCTION COMPONENT------------------------
-function App(){
-    return(
+// function App(){
+//     return(
 
-      <div>
-        <h1>Asslamoalaikum--!</h1>
-      </div>
-    )
-}
+//       <div>
+//         <h1>Asslamoalaikum--!</h1>
+//       </div>
+//     )
+// }
 
-export default App
+// export default App
+
+
+
+//Variable in class function-------
+// class App extends React.Component{
+//   render(){
+
+//     let name="Amna Munir"
+//     return(
+// //jsx mai jubbi javascript istemal kernihai tou curly braces used!!
+//       <div>
+    
+//     <h1>{name}</h1>
+//       </div>
+//     )
+//   }
+// }
+// export default App
+
+
+//parsing error---if any one mistake full page  console error for javascript
+//react auto refresh   
+
+// --variable in class function-------Concatenate templae literals and array----------------------------------
+// class App extends React.Component{
+//   render(){
+
+//     let firstname="Maher";
+//     let lastname="Zain";
+//      let array=[10,61,19]
+//     return(
+// //jsx mai jubbi javascript istemal kernihai tou curly braces used!! 
+//       <div>
+     
+//     <h1>{`${firstname} ${lastname}`}</h1>
+//   <h1>{array}</h1>
+//       </div>
+//     )
+//   }
+// }
+// export default App
+
+
+
+
+
+
+
