da5345539e93b512aff849a4be00a54f28bcc005
amnamunir-2020 <59399151+amnamunir-2020@users.noreply.github.com>
2020-10-18 01:10:25 +0500

    Class&FunctionComponent
    app.js file code component based

---
+++ appComponenet.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//CLASS COMPONENT----------------------
+// class App extends React.Component{
+//   render(){
+//     return(
+
+//       <div>
+//         <h1>Asslamoalaikum Warhmatullahi Wabarakatuhu!</h1>
+//       </div>
+//     )
+//   }
+// }
+//export default App
+
+//FUNCTION COMPONENT------------------------
+function App(){
+    return(
+
+      <div>
+        <h1>Asslamoalaikum--!</h1>
+      </div>
+    )
+}
+
+export default App
+
+//Default Code Run
+//jsx  html +javascriptin react
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+       
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Asslamoalikum ReactWorld!
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
